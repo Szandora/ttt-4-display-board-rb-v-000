fdab054158f6e2d81ab199f85e01539abdaf08fd
Sheila Wittman <szandora789@gmail.com>
2018-04-29 16:07:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,11 +10,3 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 end
-
-def display_board(x_center)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |#{board[1]}|   "
-  puts "-----------"
-  puts "   |   |   "
-end
